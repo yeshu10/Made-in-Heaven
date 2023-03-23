9ad1f44c4f7f7283c0296f933e70d22065a6243d
yeshu10 <86351169+yeshu10@users.noreply.github.com>
2023-03-23 14:50:14 +0530

    Add files via upload

--- save.php
+++ save.php
@@ -1,6 +1,6 @@
 <?php
-    
-$con=mysqli_connect("localhost","root","","wedding");
+    session_start();
+	$con=mysqli_connect("localhost","root","","wedding");
 	if(isset($_POST["fname"]))
 	{
 		$fnam=$_POST["fname"];
@@ -17,19 +17,32 @@ $con=mysqli_connect("localhost","root","","wedding");
 		include_once "Header.php";
 		echo "<script src=\"jquery.min.js\"></script>";
 		echo "<script src=\"bootstrap.js\"></script>";
-		
-		$ins="Insert into usermaster(fname,lname,mobile,email,pass,address) values('$fname','$lname','$mobile','$email','$pass','$addr')";
+		$sql="SELECT * FROM usermaster Where email='$email'";
+		$result=mysqli_query($con,$sql);
+		$present=mysqli_num_rows($result);
+		if($present>0)
+		{
+			$_SESSION['email_alert']='1';
+			header("Location:Register.php");
+		}
+		else
+		{
+			$ins="Insert into usermaster(fname,lname,mobile,email,pass,address) values('$fname','$lname','$mobile','$email','$pass','$addr')";
 			if(mysqli_query($con,$ins))
 			{
 			echo "<center><h3 style='color:Green'>Now You are registered in our site.</h3></center>";
 			}
-	         else
-			 {	
-				echo mysqli_error($con);
-				echo "<center><h3 style='color:Red'>Sorry for the inconvenience caused. Please try again after some time.</h3></center>";
+	        else
+			{	
+			echo mysqli_error($con);
+			echo "<center><h3 style='color:Red'>Sorry for the inconvenience caused. Please try again after some time.</h3></center>";
 			//echo mysqli_error($this->con);
-		 }
+		    }
+	
+	// else
+	// 	header("Location:Register.php");
+		}
 	}
-	else
-		header("Location:Register.php");
+	 else
+	 header("Location:Register.php");
 ?>
